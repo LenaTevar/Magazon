89efe969557b54abb086f0a59e503956842c24f7
HelenaDesktop <htevar@gmail.com>
2019-07-05 11:54:55 +0200

    Car mechanics part 1

---
+++ Magazon/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Car;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Car.transform.position + offset;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Magazon
 Unity game
+
+## Useful stackoverflow posts
+
+1. [Visual Studio no autocompleting (script as miscelaneous files)](https://stackoverflow.com/questions/42597501/autocompletion-not-working-in-visual-studio)
+2. [Wheel collider  tutorial](https://docs.unity3d.com/Manual/WheelColliderTutorial.html)
